767e8eedeae43daed8fd9a5da3f1e2ff463028b3
kenji <kenji256@gmail.com>
2016-05-17 00:31:14 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,83 @@
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib,"d3d9.lib")
+
+#include <windows.h>
+#include <d3d9.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+void GetPixelShaderVersion(HWND hStatic)
+{
+	TCHAR szText[256] = { 0 };
+	LPDIRECT3D9 pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+	if (pD3D)
+	{
+		D3DCAPS9 d3dCaps9;
+		pD3D->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &d3dCaps9);
+		wsprintf(szText, TEXT("ピクセルシェーダのバージョンは %d.%d です。"),
+			D3DSHADER_VERSION_MAJOR(d3dCaps9.PixelShaderVersion),
+			D3DSHADER_VERSION_MINOR(d3dCaps9.PixelShaderVersion));
+		pD3D->Release();
+		SetWindowText(hStatic, szText);
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hStatic;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hStatic = CreateWindow(TEXT("STATIC"), 0, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		GetPixelShaderVersion(hStatic);
+		break;
+	case WM_SIZE:
+		MoveWindow(hStatic, 10, 10, LOWORD(lParam) - 10, 32, TRUE);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("ピクセルシェーダーのバージョンを取得する"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
